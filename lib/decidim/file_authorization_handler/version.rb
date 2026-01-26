768642413e92c1855023e0386feca0c698020a5f
elviabth <ejbenedith@gmail.com>
2026-01-26 13:57:51 +0100

    upgrade to version 0.31

--- lib/decidim/file_authorization_handler/version.rb
+++ lib/decidim/file_authorization_handler/version.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module FileAuthorizationHandler
-    DECIDIM_VERSION = "0.30.1"
+    DECIDIM_VERSION = "0.31"
 
     # Uses the latest matching Decidim version for
     # - major, minor and patch
